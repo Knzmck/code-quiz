c5327240a91535290555fb2a29019365a692246a
Mackenzie Schutz <mshoots64@gmail.com>
2020-03-31 16:41:47 -0500

    added styling and buttons

---
+++ assets/script.js
@@ -0,0 +1,53 @@
+var startButton = document.querySelector("#startButton");
+var submitButton = document.querySelector("#submitButton")
+
+// var timer = new timer();
+// t = 60;
+// function timer() {
+//     t= t-1;
+//     if (t < 60) {
+//         time.innerHTML = t;
+//     }
+//     if (t < 1) {
+//         window.clearInterval (update);
+
+// }
+
+// }
+
+// update = setInterval("timer()" , 1000);
+
+var score =0;
+var questions = [
+    {
+        prompt: "question",
+        answer: "a"
+    },
+    {
+        prompt: "question",
+        answer: "a"
+    },
+]
+
+
+// function to start quiz (array of questions rotating on single html page)
+// when click answer add to score and add or subtract time
+// when timer ends 
+
+
+
+// <!-- 
+// Homepage to start quiz
+// question pages with a timer
+// if correct answer add time
+// if incorrect answer subtract time
+// when out of time quiz ends and score displays
+// prompt to enter initial to save score 
+// go to high score pages
+// have option to clear high score or go back to home page 
+
+// Pages: Homepage, question page, Score save page, high score page
+
+// Quiz Questions: 
+
+// -->
