dac15485cb7142061b9a7efafd13308fd7b1ede6
Mackenzie Schutz <mshoots64@gmail.com>
2020-04-02 18:22:45 -0500

    added start function

--- script.js
+++ script.js
@@ -1,46 +1,83 @@
 var startButton = document.getElementById("startbtn");
-var questionEl = document.getElementById('question-text');
-var answerEl = document.getElementById("answer-button");
+var questionEl = document.getElementById("question-text");
+var nextButton = document.getElementById("next-btn")
+var answerBtnEl = document.getElementById("answer-button");
 var homePage = document.getElementById("homepage");
 var questionContainer = document.getElementById("qcontainer");
 
-var randomQuestions
-var currentQuestionList 
+let randomQuestions, currentQuestionIndex
 
-startButton.addEventListener('click', function(startGame){
-    //hide homepage
+
+startButton.addEventListener('click', startGame)
+
+function startGame() {
+    //hides hompage
     homePage.classList.add('hide')
-    //show question page
+    //selects a random question
+    randomQuestions = questions.sort(() => Math.random() - 1)
+    //choses first question in array (0)
+    currentQuestionList = 0
+    //shows question container
     questionContainer.classList.remove('hide')
-    randomQuestions = questions.sort(() => Math.random() - .5)
-    currentQuestion = 0
+    //sets question 
     setQuestion()
-});
-console.log(startButton);
+}
 
-
+//function to display question
 function setQuestion() {
-    displayQuestion(randomQuestions[currentQuestionList])
+    //resets old question 
+    resetFunction()
+    //shows new question
+    displayQuestion(randomQuestions[currentQuestionIndex])
+    
 }
 
 function displayQuestion(question) {
-    questionEl.innerHTML = question.question
+    //links question to question text
+    questionEl.innerText = question.question
+    //links respective answers to question buttons
+    question.answers.forEach(answer => {
+        var button = document.createElement('button')
+        //displays answer on buttons
+        button.innerText = answer.text
+        button.classList.add('btn')
+        button.addEventListener('click', selectAnswer)
+        answerBtnEl.appendChild(button)
+    })
+    
+}
+//trying to reset after answer selected??
+function resetFunction() {
+    nextButton.classList.add('hide')
+    while (answerBtnEl.firstChild) {
+        answerButtonsElement.removeChild (answerBtnEl.firstChild)
+    }
+    
 }
 
-var questions = [
+function selectAnswer(event) {
+
+}
+
+
+//question array
+const questions = [
     {
-        question: 'This is a question654645',
+        question: 'This is a question654645?',
         answers: [
-            {option: 'a', correct: true },
-            {option: 'b', correct: false }
+            {text: 'a', correct: true },
+            {text: 'b', correct: false }
+
+        ]
+    },
+
+    {
+        question: 'This is a question65?',
+        answers: [
+            {text: 'a', correct: true },
+            {text: 'b', correct: false }
 
         ]
     }
 
 ]
-
-function setQuestion () {
-
-}
-
-
